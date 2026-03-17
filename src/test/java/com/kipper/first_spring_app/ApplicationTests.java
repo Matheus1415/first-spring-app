f476358a68cb5dd5e7be8c2149ca47592b5ba14b
Matheus <mps.web.tech@gmail.com>
2026-03-17 17:38:17 -0300

    feat: init projetc

---
+++ src/test/java/com/kipper/first_spring_app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipper.first_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
